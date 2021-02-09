529f416c5ff64afa5bd96eee09fa90ccf036a9ad
Evan Shortiss <evanshortiss@gmail.com>
2021-02-09 23:33:26 +0000

    chore: update formatting for codebase

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-if (typeof Reflect === 'undefined' || typeof (Reflect as any).defineMetadata === 'undefined') {
-    throw new Error('tstate-machine requires Reflect.defineMetadata to be available. Please import/require the "reflect-metadata" module prior to loading tstate-machine.')
+if (
+  typeof Reflect === 'undefined' ||
+  typeof (Reflect as any).defineMetadata === 'undefined'
+) {
+  throw new Error(
+    'tstate-machine requires Reflect.defineMetadata to be available. Please import/require the "reflect-metadata" module prior to loading tstate-machine.'
+  );
 }
 
 export { StateMachine } from './StateMachine';
